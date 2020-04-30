24c2bf2c95d4a7015421101fe4bab7b7d25d0dac
michael Zuniga <michaelzunigaa@gmail.com>
2020-04-30 13:35:47 -0700

    messe

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
 
